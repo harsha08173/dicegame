5d625465ebc1cf3d9dc96d735242fe83ea70c072
harsha08173 <98424469+harsha08173@users.noreply.github.com>
2023-06-20 03:07:34 +0530

    Updated  index.js

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ var num2 = Math.floor(Math.random()*6)+1;
 
 var s="";
 
-document.getElementsByClassName("img1")[0].setAttribute('src','./images/dice'+num1+'.png');
-document.getElementsByClassName("img2")[0].setAttribute('src','./images/dice'+num2+'.png');
-console.log('images/dice'+num1+'.png');
-console.log('images/dice'+num2+'.png');
+document.getElementsByClassName("img1")[0].setAttribute('src','./dice'+num1+'.png');
+document.getElementsByClassName("img2")[0].setAttribute('src','./dice'+num2+'.png');
+
 if (num1>num2) {
     s = '🏁Player 1 Wins';
 
